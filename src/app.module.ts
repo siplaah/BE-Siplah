de028a9319cffa168d3eb1738c9c5193be64ad5f
Ajengwahyu <ajengwahyu@student.uns.ac.id>
2024-07-08 11:44:13 +0700

    email

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,35 @@ import { PresensiModule } from './presensi/presensi.module';
 import { TimeOffModule } from './time_off/time_off.module';
 import { MeetingModule } from './meeting/meeting.module';
 import { AssessmentModule } from './assessment/assessment.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailerOptions } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [JabatanModule, EmployeeModule, AuthModule, ProjectModule, KeyResultModule, OvertimeModule, DailyReportModule, PresensiModule, TimeOffModule, MeetingModule, AssessmentModule],
+  imports: [
+    JabatanModule,
+    EmployeeModule,
+    AuthModule,
+    ProjectModule,
+    KeyResultModule,
+    OvertimeModule,
+    DailyReportModule,
+    PresensiModule,
+    TimeOffModule,
+    MeetingModule,
+    AssessmentModule,
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: 'ajengwahyu@gmail.com', // Ganti dengan email Gmail Anda
+          pass: 'zmtr aodl gdog wqzs', // Ganti dengan password Gmail Anda atau app-specific password
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>', // Ganti dengan alamat email pengirim default
+      },
+    } as MailerOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/meeting/meeting.service.ts
+++ src/meeting/meeting.service.ts
@@ -3,14 +3,18 @@ import { CreateMeetingDto } from './dto/create-meeting.dto';
 import { UpdateMeetingDto } from './dto/update-meeting.dto';
 import { PrismaService } from 'src/prisma.service';
 import { Prisma } from '@prisma/client';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MeetingService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   async create(createMeetingDto: CreateMeetingDto) {
-    const dateMeeting = new Date(createMeetingDto.date)
-    
+    const dateMeeting = new Date(createMeetingDto.date);
+
     if (isNaN(dateMeeting.getTime())) {
       throw new BadRequestException('Invalid date format');
     }
@@ -33,10 +37,62 @@ export class MeetingService {
         },
         include: { meetingEmployees: true },
       });
+
+      await this.sendMeetingNotification(createMeeting);
+
       return createMeeting;
     } catch (error) {
       console.error('Error creating meeting:', error);
       throw new BadRequestException('Gagal menambahkan meeting');
+    }
+  }
+
+  private async sendMeetingNotification(meeting) {
+    const { description, date, meetingEmployees } = meeting;
+
+    if (!meetingEmployees || !Array.isArray(meetingEmployees)) {
+      throw new Error('Invalid meetingEmployees data');
+    }
+
+    // Ambil semua ID employee dari meetingEmployees
+    const id_employee = meetingEmployees.map((me) => me.id_employee);
+
+    try {
+      // Query Prisma untuk mengambil informasi karyawan berdasarkan ID
+      const employees = await this.prisma.employee.findMany({
+        where: {
+          id_employee: {
+            in: id_employee, // Mengambil karyawan dengan ID yang terdaftar dalam employeeIds
+          },
+        },
+      });
+
+      // Dapatkan email dari setiap karyawan yang terkait
+      const employeeEmails = employees.map((employee) => employee.email);
+
+      // Kirim email notifikasi ke setiap karyawan yang terlibat dalam pertemuan
+      await Promise.all(
+        employeeEmails.map(async (email) => {
+          try {
+            await this.mailerService.sendMail({
+              to: email,
+              subject: 'Pertemuan Baru: ' + description,
+              text: `Anda telah dijadwalkan untuk pertemuan baru pada tanggal ${new Date(
+                date,
+              ).toLocaleDateString()}.`,
+            });
+            console.log(`Email notifikasi dikirim ke: ${email}`);
+          } catch (error) {
+            console.error(
+              `Gagal mengirim email notifikasi ke: ${email}`,
+              error,
+            );
+          }
+        }),
+      );
+    } catch (error) {
+      console.error('Error retrieving employee data:', error);
+      throw new Error('Gagal mengambil data karyawan');
     }
   }
 
@@ -63,24 +119,24 @@ export class MeetingService {
     updateMeetingDto: UpdateMeetingDto,
   ) {
     const { id_employee, ...meetingData } = updateMeetingDto;
-    const dateMeeting = new Date(updateMeetingDto.date)
-    
+    const dateMeeting = new Date(updateMeetingDto.date);
+
     if (isNaN(dateMeeting.getTime())) {
       throw new BadRequestException('Invalid date format');
     }
     const isoDate = dateMeeting.toISOString();
-    
+
     try {
       const updateData: Prisma.MeetingUpdateInput = {
         ...meetingData,
         date: isoDate,
         meetingEmployees: {
           connect: id_employee.map((id) => ({
-            id_meeting_employee: id, 
+            id_meeting_employee: id,
           })),
         },
       };
-  
+
       const updateMeeting = await this.prisma.meeting.update({
         where: where,
         data: updateData,
@@ -94,7 +150,6 @@ export class MeetingService {
       throw error;
     }
   }
-  
 
   async remove(id: number) {
     try {
