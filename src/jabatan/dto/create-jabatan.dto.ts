d4dc183e9635c7a756619821f3493570f5cfd687
daaniispramd <92668069+daaniis@users.noreply.github.com>
2024-08-08 18:07:07 +0700

    update Jabatan uhuyy dah makan belom?

--- src/jabatan/dto/create-jabatan.dto.ts
+++ src/jabatan/dto/create-jabatan.dto.ts
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
 
-import { IsString } from "class-validator";
+import { IsInt, IsOptional, IsString } from "class-validator";
 
 export class CreateJabatanDto {
     @IsString()
     name_jabatan: string;
+
+    @IsOptional()
+    @IsInt()
+    parentId?: number;
 }
